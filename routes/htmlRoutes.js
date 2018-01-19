bd096554c6cb36df5e5d9babdd152e0c55efebe9
dbrudner <dbrudne1@gmail.com>
2018-01-19 17:59:01 -0500

    commit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,19 +2,23 @@ const path = require('path');
 
 function hello(app) {
 	app.get("/", function(req, res) {
-	  res.sendFile(path.join(__dirname, "../public/index.html"));
+		res.sendFile(path.join(__dirname, "../public/index.html"));
 	});
 
 	app.get("/suggestion", function(req, res) {
-	  res.sendFile(path.join(__dirname, "../public/suggestion.html"));
+		res.sendFile(path.join(__dirname, "../public/suggestion.html"));
 	});
 
 	app.get("/view", function(req, res) {
-	  res.sendFile(path.join(__dirname, "../public/view.html"));
+		res.sendFile(path.join(__dirname, "../public/view.html"));
 	});
 
 	app.get("/add", function(req, res) {
-	  res.sendFile(path.join(__dirname, "../public/add.html"));
+		res.sendFile(path.join(__dirname, "../public/add.html"));
+	});
+
+	app.get('*', function (req, res) {
+		res.redirect('/');
 	});
 }
 
